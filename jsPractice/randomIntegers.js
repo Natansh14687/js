b84a3b37ffd59776baf27d7719543a26f9e76b93
Natansh14687 <natansh522@gmail.com>
2024-05-19 17:17:39 +0530

    5/19/2024

---
+++ jsPractice/randomIntegers.js
@@ -0,0 +1,17 @@
+//--------------------------------- Random Integers from 1 to 10 ------------------------------------
+// console.log(Math.random());
+// console.log();
+// console.log(Math.random()*10);
+// console.log();
+// console.log(Math.floor(Math.random()*10));
+// console.log();
+// console.log(Math.floor(Math.random()*10)+1);
+
+let step1 = Math.random();
+let step2 = step1 * 10;
+let step3 = Math.floor(step2);
+let step4 = step3 + 1;
+console.log(step1)
+console.log(step2)
+console.log(step3)
+console.log(step4)
