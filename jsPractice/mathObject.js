da60b86bb1fc76d1ca944998f8743c044e51b2d9
Natansh14687 <natansh522@gmail.com>
2024-05-19 16:52:29 +0530

    5/19/2024

---
+++ jsPractice/mathObject.js
@@ -0,0 +1,16 @@
+console.log(Math.abs(12));
+console.log(Math.abs(-12))
+console.log(Math.pow(2,5));
+console.log(Math.floor(6));
+console.log(Math.floor(6.9999999999999));
+console.log(Math.floor(-6.11111))
+console.log();
+console.log(Math.ceil(6));
+console.log(Math.ceil(6.00000000000001));
+console.log(Math.ceil(-6.99999999999))
+console.log();
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
