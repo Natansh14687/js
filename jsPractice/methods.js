9b994eba6bb1a4acf17896e9b9aab3beb038aaa2
Natansh14687 <natansh522@gmail.com>
2024-05-20 19:29:06 +0530

    Everything in js is object

---
+++ jsPractice/methods.js
@@ -0,0 +1,31 @@
+// const calculator = {
+//     add : function(a,b){
+//         return a+b;
+//     },
+//     sub : function(a,b){
+//         return a - b;
+//     },
+//     mul : function(a,b){
+//         return a * b;
+//     },
+//     div : function(a,b){
+//         return a/b;
+//     },
+// }
+
+// --------------------------------------- methods shorthand -------------------------------
+
+const cal = {
+    add(a,b){
+        return a+b;
+    },
+    sub(a,b){
+        return a-b;
+    },
+    mul(a,b){
+        return a*b;
+    },
+    div(a,b){
+        return a/b;
+    }
+}
