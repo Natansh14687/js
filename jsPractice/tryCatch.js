c1537a2bd8242b367118cf617caa2d8c646c55a0
Natansh14687 <natansh522@gmail.com>
2024-05-22 16:45:02 +0530

    5/22/2024

---
+++ jsPractice/tryCatch.js
@@ -0,0 +1,12 @@
+console.log("Hello, there");
+console.log("Hello, there");
+console.log("Hello, there");
+try{
+    console.log(a);
+} catch{
+    console.log("this is wrong way")
+}
+console.log("Hello, there");
+console.log("Hello, there");
+console.log("Hello, there");
+console.log("Hello, there");
