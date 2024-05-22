a21052b418e82c27680cdbca2c877c29b503699d
Natansh14687 <natansh522@gmail.com>
2024-05-22 17:55:48 +0530

    5/22/2024

---
+++ jsPractice/arrowFunction.js
@@ -0,0 +1,21 @@
+const sum = (a,b) => {
+    console.log(a+b);
+}
+
+sum(1,2)
+
+// ----------- Single parameter can be pass without curly braces -----------------
+
+const print = n => {
+    console.log(n);
+}
+
+print(2);
+
+// ------------- we have to use parenthesis whether if we are giving parameter or not -----------
+
+const hello = () => {
+    console.log("hello, world");
+}
+
+hello();
