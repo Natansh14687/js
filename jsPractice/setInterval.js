3cae4a398558c8f048a4af55784dcae6e1747ae4
Natansh14687 <natansh522@gmail.com>
2024-05-22 20:26:22 +0530

    5/22/2024

---
+++ jsPractice/setInterval.js
@@ -0,0 +1,15 @@
+// console.log("heya");
+
+const id = setInterval(() => {
+    console.log("Natansh")
+},2000);
+
+console.log("heya");
+
+// console.log(id);
+
+setInterval(() => {
+    console.log("dushyant");
+}, 3000);
+
+clearInterval(id);
