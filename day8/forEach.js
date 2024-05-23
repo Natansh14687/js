6d9aeca3560e6470d3425c7fc1ee8894b8a68a88
Natansh14687 <natansh522@gmail.com>
2024-05-23 11:34:21 +0530

    5/23/2024

---
+++ day8/forEach.js
@@ -0,0 +1,23 @@
+const arr = [1,2,3,4,5,6];
+
+arr.forEach(function(ele){
+    console.log(ele);
+});
+
+stdArr = [{
+    Name : "Natansh",
+    Age : 20,
+},{
+    Name : "Dushyant",
+    Age : 19, 
+},{
+    Name : "Rohit",
+    Age : 19,
+},{
+    Name : "Hemant",
+    Age : 20,
+}];
+
+stdArr.forEach((ele) => {
+    console.log(ele.Name);
+});
