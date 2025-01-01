1ade6e83fd8237ba32afbaaf3ca0396ab776c070
Natansh Khurana <natansh522@gmail.com>
2025-01-01 21:13:29 +0530

    js on the go

---
+++ AnuragSirJs/DOM/script.js
@@ -0,0 +1,13 @@
+// const h1 = document.getElementsByTagName("h1");
+// console.log(h1);
+
+// const var2 = document.getElementsByClassName("hii");
+// console.log(var2);
+
+// const var3 = document.getElementById("title-one");
+// console.log(var3);
+// var3.outerText = "ioi"; // but we don't use this.
+
+const title = document.querySelectorAll()
+
+
